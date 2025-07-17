2e8dcbe359316920cfc4fb0d6b3cf70ea0036af4
ksanthiana <s.kaze@alustudent.com>
2025-07-17 14:17:12 +0200

    this is my output

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,34 +5,60 @@
 import requests
 import sys
 
+
+
 if __name__ == "__main__":
-        employee_id = int(sys.argv[1])
+    employee_Id = int(sys.argv[1])
 
-        
+    todo_url = "https://jsonplaceholder.typicode.com/todos"
+    user_data_url = "https://jsonplaceholder.typicode.com/users"
+
+    user_response = requests.get(user_data_url)
+    todo_response = requests.get(todo_url)
+    # if todo_response.status_code & user_response.status_code == 200:
+    todos = todo_response.json()
+    users = user_response.json()
+    for user in users:
+        if user.get("id") == employee_Id:
+            employee_name = user.get("name")
+    # filter completed tasks
+    done = []
+    total = 0
+    completed = 0
+    for todo in todos:
+        if todo.get("userId") == employee_Id:
+            total += 1
+            if todo.get("completed"):
+                completed += 1
+                done.append(todo.get("title"))
+    # Display the progress information
+    print(f"Employee {employee_name} is done with tasks({completed}/{total}):")
+    for _ in done:
+        print(f"\t {_}")
 
 
-        user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-        todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
 
-        
 
-        user_response = requests.get(user_url)
-        todos_response = requests.get(todos_url)
+
+
+
+        #user_response = requests.get(user_url)
+        #todos_response = requests.get(todos_url)
 
                                 
         
-        user_data = user_response.json()
-        todos_data = todos_response.json()
+        #user_data = user_response.json()
+       # todos_data = todos_response.json()
 
                                         
         
-        employee_name = user_data.get("name")
-        total_tasks = len(todos_data)
-        done_tasks = [task for task in todos_data if task.get("completed")]
-        number_done_tasks = len(done_tasks)
+      #  employee_name = user_data.get("name")
+     #   total_tasks = len(todos_data)
+    #    done_tasks = [task for task in todos_data if task.get("completed")]
+   #     number_done_tasks = len(done_tasks)
 
                                                                 
         
-        print(f"Employee {employee_name} is done with tasks({number_done_tasks}/{total_tasks}):")
-        for task in done_tasks:
-            print(f"\t {task.get('title')}")
+#print(f"Employee {employee_name} is done with tasks({number_done_tasks}/{total_tasks}):")
+ #       for task in done_tasks:
+  #          print(f"\t {task.get('title')}")
