89e556401f8978e2e4f7c5ec2b17ad43a55de1a3
ksanthiana <s.kaze@alustudent.com>
2025-07-17 14:28:20 +0200

    this is my output

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -35,30 +35,3 @@ if __name__ == "__main__":
     print(f"Employee {employee_name} is done with tasks({completed}/{total}):")
     for _ in done:
         print(f"\t {_}")
-
-
-
-
-
-
-
-        #user_response = requests.get(user_url)
-        #todos_response = requests.get(todos_url)
-
-                                
-        
-        #user_data = user_response.json()
-       # todos_data = todos_response.json()
-
-                                        
-        
-      #  employee_name = user_data.get("name")
-     #   total_tasks = len(todos_data)
-    #    done_tasks = [task for task in todos_data if task.get("completed")]
-   #     number_done_tasks = len(done_tasks)
-
-                                                                
-        
-#print(f"Employee {employee_name} is done with tasks({number_done_tasks}/{total_tasks}):")
- #       for task in done_tasks:
-  #          print(f"\t {task.get('title')}")
