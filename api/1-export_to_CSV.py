55e11c75ac8b8fa9caed5b652d69330afc607689
ksanthiana <s.kaze@alustudent.com>
2025-07-21 20:27:42 +0200

    this is my output

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
-"""Exports tasks of an employee to a CSV file"""
-
+''' Test request to parse API's
+'''
 import csv
 import requests
 import sys
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
-    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id)
-
-    user_response = requests.get(user_url)
-    todos_response = requests.get(todos_url)
-
-    username = user_response.json().get("username")
-    todos = todos_response.json()
-
-    filename = "{}.csv".format(user_id)
-
-    with open(filename, mode='w', newline='') as csvfile: 
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in todos:
-            writer.writerow([user_id,
-                             username,
-                             task.get("completed"),
-                             task.get("title")])
+    if len(sys.argv) > 1 and sys.argv[1].isdigit():
+        api_endpoint = "https://jsonplaceholder.typicode.com"
+        user_id = sys.argv[1]
+        user_data = requests.get(api_endpoint + "/users/" + user_id).json()
+        username = user_data.get('username')
+        todo_data = \
+            requests.get(api_endpoint + "/users/" + user_id + "/todos").\
+            json()
+        with open("{}.csv".format(user_id), 'w') as csv_file:
+            for task in todo_data:
+                csv_file.write('"{}","{}","{}","{}"\n'.format(
+                    user_id,
+                    username,
+                    task.get('completed'),
+                    task.get('title')
+                ))
